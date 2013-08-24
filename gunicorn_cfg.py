7e4eacae88a4a8d896d6df4918fa7d57ef752ef9
Akshar Raaj <akshar@agiliq.com>
2013-08-24 16:47:39 +0530

    Create gunicorn configuration file

---
+++ gunicorn_cfg.py
@@ -0,0 +1,8 @@
+import multiprocessing
+
+bind = '127.0.0.1:8001'
+workers = multiprocessing.cpu_count() * 2 + 1
+daemon = True
+pidfile = 'gunicorn_process_id.txt'
+django_settings = 'test_project.settings'
+pythonpath = '.'
