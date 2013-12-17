2dc3af3bbe0401c6afa2b02f362c8daa5fe82d5a
Akshar Raaj <akshar@agiliq.com>
2013-12-17 20:14:26 +0530

    Add accesslog and errorlog for gunicorn

--- gunicorn_cfg.py
+++ gunicorn_cfg.py
@@ -6,3 +6,5 @@ daemon = True
 pidfile = 'gunicorn_process_id.txt'
 django_settings = 'test_project.settings'
 pythonpath = '.'
+accesslog = 'gunicorn_access_log.txt'
+errorlog = 'gunicorn_error_log.txt'
