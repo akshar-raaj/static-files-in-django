92aee071fed0eaad28a51eae00b612d226c94c8d
Akshar Raaj <akshar@agiliq.com>
2013-12-26 14:17:55 +0530

    Added caching

--- some_app/urls.py
+++ some_app/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from django.views.decorators.cache import cache_page
 
 from .views import HomeView
 
 urlpatterns = patterns('',
-    url(r'^home$', HomeView.as_view()),
+    url(r'^home$', cache_page(HomeView.as_view(), 60)),
 )
--- test_project/settings/common.py
+++ test_project/settings/common.py
@@ -142,3 +142,9 @@ LOGGING = {
         },
     }
 }
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
