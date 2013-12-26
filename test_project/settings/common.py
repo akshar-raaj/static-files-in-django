be01006d457448fb6f3f6728656fecc73249dc6c
Akshar Raaj <akshar@agiliq.com>
2013-12-26 14:32:10 +0530

    remove caching

--- test_project/settings/common.py
+++ test_project/settings/common.py
@@ -142,9 +142,3 @@ LOGGING = {
         },
     }
 }
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
